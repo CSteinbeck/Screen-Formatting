f94f8d56e9d07fd2d3e4f0960f05c081aa659044
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2020-11-09 19:12:16 -0500

    -NEW LISP

--- Lisp/sf.lisp
+++ Lisp/sf.lisp
@@ -1,51 +1,39 @@
 #!/usr/bin/sbcl --script
 ;;;Colton Steinbeck
 ;;;Screen Formatting in Lisp;;;
-(defvar line)
-(defvar charCount)
-(defvar charCount 0)
-(defvar wordCount)
-(setf wordCount 0)
-(defvar lineCount)
-(setf lineCount 1)
-(defvar originalFile)
+(defvar fl) ;; Command-Line arg that will be looped through
+(defvar instance "");;Char item that is monitored
+(defvar i 0) ;;Sets the index to zero for looping
+(defvar j 0)
+(defvar words)
 (defvar long_string)
-;;(defvar prev)
-;;(setf prev #\p )
-(defvar index)
-(setf index 0)
+(defvar line "") ;;line item that the chars are inserted into
+(defvar lineMin)
+(defvar lineMax)
+(defvar charCount 0) ;;# of chars taken in
+(defvar lineCount 1);; # of lines counted 
+(defvar wordcount 0) ;; Word count for the new line
 ;;;;;;;;;;;;;;
 ;;;;;;;;;;;Input Sequence for the file
-(setq originalFile (open (car(cdr *posix-argv* ) ) ) );;;;;;;;;;;;Opens the given file that the user inputs
-      (setf long_string (make-string (file-length originalFile))) ;;Sets the new string size to the size of the originalFile
-      (loop for instance = (read-char originalFile nil :eof ) ; stream, no error, :eof value
-                  until (eq instance :eof )
-                  do
-                  ;(print instance)
-                  (cond 
-                        ((or ( char> instance #\9 ) (char< instance #\0 ) )
-                  
-                        (cond
-                              ((and(char/= instance #\NewLine )(char/= instance #\Space )
-                              (setf(aref originalFile index ) long_string)    
-                              (setf prev instance) ;;Setting previous to current
-                              (setf index (+ index 1))))
-                        ) ;;End of the Loop
-                                              
-                        
-                        
-                        
-                        
-                        )    
-                        
-                        
-                  
-                  )
+(setq fl (open (car(cdr *posix-argv* ) ) ) );;;;;;;;;;;;Opens the given file that the user inputs
+(setq long_string (make-string (file-length fl))) ;;Sets the new string size to the size of the originalFile
+(setq line(length long_string))
+           (loop for instance = (read-char fl nil :eof) ; stream, no error, :eof value
+                  until (eq instance :eof)
+                        do
+                        (cond ((or (char> instance #\9 ) (char< instance #\0 ) )
+                              (progn
+                              ;;(write-line "Test")
+                              (setf (aref long_string i) instance)
+                              (incf i)
+                              (princ instance) 
+                              ))
+                       )            
+            )
 
-  
-      ) ;;end of instance loop
-
-princ(long_string)
+;;(write instance)          
+;;(format t "~a" line)
+(close fl)
 ;;(format t "The number of lines is ~2d" lineCount)
 ;;(terpri)
 ;;(close fl)
